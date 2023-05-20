0b141b60e3b6786fa7bf60e8420e41facff59977
Enes-38 <134143592+Enes-38@users.noreply.github.com>
2023-05-20 14:31:43 +0200

    Add files via upload

---
+++ enes38-iprg-b04-a01/enes38-iprg-b04-a01/introprog_buecherliste.c
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "introprog_structs_lists_input.h"
+
+#define MAX_STR 255
+
+/* Bewirkt, dass statt 'struct _element' auch 'element' verwendet
+ * werden kann.
+ */
+typedef struct _element element; 
+
+/* Bewirkt, dass statt 'struct _list' auch 'list' verwendet werden
+ * kann.  Hier in einem geschrieben, so dass man auch 'list'
+ * innerhalb der struct-Definition selbst verwenden kann.
+ */
+typedef struct _list { /* Separater Wurzelknoten */
+    element *first;    /* Anfang/Kopf der Liste */
+    int count;         /* Anzahl der Elemente */
+} list;
+
+/* HIER struct _element implementieren. */
+typedef struct _element {       //Deklerationen
+	    char title[MAX_STR];
+	    char author[MAX_STR];
+	    int year;
+	    long long int isbn;
+	    struct _element *next;
+}element;
+
+/* Fuege ein Element am Anfang der Liste an, sodass das neue
+ * Element immer das erste Element der Liste ist.  Wenn die Liste
+ * leer ist, soll das Element direkt an den Anfang platziert
+ * werden.
+ *
+ * first    - Pointer auf das erste Element (bzw. Anfang) der
+ * Liste
+ * new_elem - Pointer auf das neues Element das in die Liste
+ * eingefuegt werden soll
+ *
+ * Gib einen Pointer auf den neuen Anfang der Liste zurueck.
+ */
+element *insert_at_begin(element *first, element *new_elem) {
+    /* HIER implementieren. */
+	if (first == NULL) {   //wenn erster Elem 0 ist, dann
+		first = new_elem;   //erster = neues element
+	} else {
+		new_elem->next = first;  //sonst neues Elemt wird zu erstes Elem
+	}
+	return new_elem;
+}
+
+/* Kreiere ein neues Element mit dynamischem Speicher.
+ *
+ * title  - Titel des Buches
+ * author - Autor des Buches
+ * year   - Erscheinungsjahr des Buches
+ * isbn   - ISBN des Buches
+ *
+ * Gib einen Pointer auf das neue Element zurueck.
+ */
+element *construct_element(char *title, char* author, int year, long long int isbn) {
+    /* HIER implementieren. */
+	element *neu_element = (element*) malloc(sizeof(element));
+	
+	strncpy(neu_element->title, title, MAX_STR);
+	neu_element->title[254]= '\0';
+	
+	strncpy(neu_element->author, author, MAX_STR);
+	neu_element->author[254]= '\0';
+	
+	neu_element->year = year;
+	neu_element->isbn = isbn;
+	neu_element->next = NULL;
+	return neu_element;
+	
+}
+
+/* Gib den der Liste und all ihrer Elemente zugewiesenen
+ * Speicher frei.
+ */
+void free_list(list *alist) {    //Hier wird Speicher freigegeben
+    /* HIER implementieren. */
+	element *new = alist->first;
+	while (new != NULL) {
+		element *save = new;
+		new = new->next;
+		free(save);     //*save freigegeben
+		
+	}
+	free(alist);        //alist freigegeben
+}
+
+/* Lese die Datei ein und fuege neue Elemente in die Liste ein 
+ * _Soll nicht angepasst werden_
+ */
+void read_list(char* filename, list *alist) {
+    element* new_elem;
+
+    char* title;
+    char* author;
+    int year;
+    long long int isbn;
+    read_line_context ctx;
+    open_file(&ctx, filename);
+    while(read_line(&ctx, &title, &author, &year, &isbn) == 0) {
+        new_elem = construct_element(title, author, year, isbn);
+        alist->first = insert_at_begin(alist->first, new_elem);
+        alist->count++;
+    }
+}
+
+/* Erstelle die Liste:
+ *  - Weise ihr dynamischen Speicher zu 
+ *  - Initialisiere die enthaltenen Variablen
+ * _Soll nicht angepasst werden_
+ */
+list* construct_list() {
+    list *alist = malloc(sizeof(list));
+    alist->first = NULL;
+    alist->count = 0;
+    return alist;
+}
+
+/* Gib die Liste aus:
+ * _Soll nicht angepasst werden_
+ */
+void print_list(list *alist) {
+    printf("Meine Bibliothek\n================\n\n");
+    int counter = 1;
+    element *elem = alist->first;
+    while (elem != NULL) {
+        printf("Buch %d\n", counter);
+        printf("\tTitel: %s\n", elem->title);
+        printf("\tAutor: %s\n", elem->author);
+        printf("\tJahr:  %d\n", elem->year);
+        printf("\tISBN:  %lld\n", elem->isbn);
+        elem = elem->next;
+        counter++;
+    }
+}
+
+/* Main Funktion
+ * _Soll nicht angepasst werden_
+ */
+int main(int argc, char** argv) {
+    list *alist = construct_list();
+    read_list(argc>1?argv[1]:"buecherliste.txt", alist);
+    print_list(alist);
+    free_list(alist);
+    return 0;
+}
